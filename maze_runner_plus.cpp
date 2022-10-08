a1db5df60d78119142f37c9b127dd2fe6347fb7b
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-07 23:04:43 -0400

    Update maze_runner_plus.cpp

--- maze_runner_plus.cpp
+++ maze_runner_plus.cpp
@@ -1,1 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 1 Task C
+I wrote a program that can evaluate multiple direction lists. Let’s use the semicolon symbol that must be used at the end of each direction in the input.
+*/
 
